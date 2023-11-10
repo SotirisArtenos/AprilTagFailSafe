4c6ab766204a8ec086e21350efa6264b680899d9
SotirisArtenos <148920126+SotirisArtenos@users.noreply.github.com>
2023-11-10 11:36:30 -0800

    Math for finding distance and angle from AprilTag

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        double h4 = 5.5; // value for distance
+        double theta = 41; // value for angle
 
+
+        theta = Math.toRadians(theta);
+        double c = h4 * Math.sin(theta);
+        double f = 6-c;
+        double d = h4 * Math.cos(theta);
+
+        double theta3 = Math.atan(f/d);
+        theta3 = Math.toDegrees(theta3); // finds degree from desired AprilTag
+        double h5 = Math.sqrt(d*d + f*f); // finds distance of robot to AprilTag
+
+
+        System.out.println("h5: " + h5 + " theta3: " + theta3);
     }
 }
